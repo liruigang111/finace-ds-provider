51bcd33d79cfe060a8c01f6948ee72061e6105a7
liruigang111 <45100762+liruigang111@users.noreply.github.com>
2018-11-24 19:55:42 +0800

    测试怎么提交

--- src/main/java/com/sdhs/finace/finacedsprovider/web/TestController.java
+++ src/main/java/com/sdhs/finace/finacedsprovider/web/TestController.java
@@ -25,4 +25,8 @@ public class TestController {
     public void printGG(){
         System.out.println("yayay");
     }
+    @RequestMapping("gfff")
+    public void printHH(){
+        System.out.println("heiehi");
+    }
 }
