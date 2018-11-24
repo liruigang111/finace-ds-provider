845b3843f75df372df98ce18260977af645ef298
liruigang111 <45100762+liruigang111@users.noreply.github.com>
2018-11-24 19:13:46 +0800

    Initial commit

---
+++ src/main/java/com/sdhs/finace/finacedsprovider/FinaceDsProviderApplication.java
@@ -0,0 +1,12 @@
+package com.sdhs.finace.finacedsprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaceDsProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinaceDsProviderApplication.class, args);
+    }
+}
---
+++ src/main/java/com/sdhs/finace/finacedsprovider/web/TestController.java
@@ -0,0 +1,18 @@
+package com.sdhs.finace.finacedsprovider.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @Value("${server.port}")
+    private String port;
+    @PostMapping(value="/port")
+    public void getPort(){
+        System.out.println("端口号是---"+port);
+    }
+}
